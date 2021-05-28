ff2c49a4022fc67a29ea715b9946b1edb9422e39
Gabriel Desktop <gabrielfernandez@knights.ucf.edu>
2021-05-28 14:44:23 -0400

    Added comments

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -3,22 +3,26 @@ import java.util.Scanner;
 
 public class App 
 {
+    // Main function
     public static void main( String[] args )
     {
+        // Prompt for user input and store in input
         System.out.println("What is the input string?");
         Scanner input = new Scanner(System.in);
+        // I'm sure I didn't have to store inside a String value, but
+        // I did anyways since I thought it would be easier
         String word = input.next();
 
+        // Call for countCharacters function within print statement
         System.out.println(word + " has " + countCharacters(word) + " characters.");
     }
 
+    // Returns int value of how many characters are in the string
     public static int countCharacters(String word) {
         int charCounter = 0;
 
         for (int i = 0; i < word.length(); i++) {
-            if (word.charAt(i) != ' ') {
-                charCounter++;
-            }
+            charCounter++;
         }
 
         return charCounter;
